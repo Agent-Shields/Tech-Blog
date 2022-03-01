33fcb480f40c522a2758cfcfd3967e863a8d946b
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-02-28 19:27:46 -0600

    setup node packages, defined db schema

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+// add more sequelize dependent code here later like session information
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers })
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'))
+})
